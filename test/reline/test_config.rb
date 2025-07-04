ecad26fcd392638553740387da6b2b76532149a7
David Rodríguez <deivid.rodriguez@riseup.net>
2025-07-04 12:23:43 +0200

    More debug

--- test/reline/test_config.rb
+++ test/reline/test_config.rb
@@ -18,7 +18,7 @@ class Reline::Config::Test < Reline::TestCase
 
   def teardown
     Dir.chdir(@pwd)
-    puts $LOADED_FEATURES.find {|f| f.end_with?("fileutils.rb") }
+    puts $LOADED_FEATURES.select {|f| f.end_with?("fileutils.rb") }
     FileUtils.rm_rf(@tmpdir)
     Reline.test_reset
     @config.reset
